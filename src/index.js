5106f01da1dc5f6d53f5a15c933a6c7d354c3cd0
unknown <marifrahmawan@gmail.com>
2023-09-01 17:22:07 +0800

    fetching data and learning redux thunk

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import AppReduxThunk from './AppReduxThunk';
 
 import { store } from './store/index';
 import { Provider } from 'react-redux';
-import AppReduxThunk from './AppReduxThunk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <AppReduxThunk />
+    {/* <AppReduxThunk /> */}
+    <App />
   </Provider>
 );
